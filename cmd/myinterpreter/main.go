3382d43aec8634136a71f80e6aa4b50e49da0486
Sparsh Kandpal <sparsh.kandpal@limechat.ai>
2025-01-22 02:33:53 +0530

    OL4/Scanning paranthesis

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -16,21 +16,31 @@ func main() {
 
 	command := os.Args[1]
 
+	const (
+		LEFT_PAREN  rune = '('
+		RIGHT_PAREN rune = ')'
+	)
+	
 	if command != "tokenize" {
 		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
 		os.Exit(1)
 	}
 
 	filename := os.Args[2]
-	fileContents, err := os.ReadFile(filename)
+	rawfileContents, err := os.ReadFile(filename)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
 		os.Exit(1)
 	}
 	
-	if len(fileContents) > 0 {
-		panic("Scanner not implemented")
-	} else {
-		fmt.Println("EOF  null") // Placeholder, remove this line when implementing the scanner
+	fileContents := string(rawfileContents)
+	for _, current := range fileContents {
+		switch current {
+		case LEFT_PAREN:
+			fmt.Println("LEFT_PAREN ( null")
+		case RIGHT_PAREN:
+			fmt.Println("RIGHT_PAREN ) null")
+		}
 	}
+	fmt.Println("EOF  null")
 }
